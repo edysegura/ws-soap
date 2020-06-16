c5c42f5bf0047a8b860edfa06659352b99870292
Edy Segura <edysegura@gmail.com>
2020-06-16 17:37:41 -0300

    Refactor cep.js

--- nodejs/cep.js
+++ nodejs/cep.js
@@ -1,10 +1,12 @@
 const soap = require('soap');
 
 const url = 'https://apphom.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl';
-const args = { cep: '37540000' };
 
-(async () => {
+async function getAddressInfo (cep = '37540000') {
+  const args = { cep };
   const client = await soap.createClientAsync(url);
   const [ result ] = await client.consultaCEPAsync(args);
-  console.log(result);
-})();
+  return result;
+};
+
+module.exports = getAddressInfo;
