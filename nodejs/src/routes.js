5e314f5a738d65df8292cf04ce0908adf8b9aa04
Edy Segura <edysegura@gmail.com>
2020-06-22 09:53:08 -0300

    Remove a blank line

--- nodejs/src/routes.js
+++ nodejs/src/routes.js
@@ -2,7 +2,6 @@ import express from 'express';
 import CepApi from './api/CepApi.js';
 
 const routes = express.Router();
-
 routes.get('/', CepApi.index);
 routes.get('/v1/cep/:cep', CepApi.getAddressInfo);
 
